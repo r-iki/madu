271117758b778385846dbe852abf2abcc3f5eb75
r-iki <riki.121110019@student.itera.ac.id>
2025-02-09 15:55:04 +0700

    Register SpectralReading model in admin interface with list display and filter options

--- src/sensors/admin.py
+++ src/sensors/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import SpectralReading
+
 class SpectralReadingAdmin(admin.ModelAdmin):
     list_display = ('timestamp', 'name',)
     list_filter = ('name',)
+
 admin.site.register(SpectralReading, SpectralReadingAdmin)
