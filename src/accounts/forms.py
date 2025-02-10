b51dbe79b986a64636baf059c0c5f4917d368490
r-iki <riki.121110019@student.itera.ac.id>
2025-02-10 16:50:21 +0700

    Remove unused files and directories from the project

--- src/accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Profile
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['bio', 'image']  # sesuaikan dengan field yang Anda miliki di model Profile
--- src/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import profile_view, edit_profile
-
-urlpatterns = [
-    path('profile/', profile_view, name='profile'),
-    path('profile/edit/', edit_profile, name='edit_profile')
-]
