a0ff650eaadf916d4d47f8458bcfb6dc1ad410e6
r-iki <riki.121110019@student.itera.ac.id>
2025-02-10 17:13:29 +0700

    Remove all __pycache__ files to clean up the project

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: cd src && daphne core.asgi:application --port $PORT --bind 0.0.0.0 --proxy-headers
--- src/core/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
-from dashboard.consumers import SensorConsumer
-
-application = ProtocolTypeRouter({
-    "websocket": URLRouter([
-        path("ws/sensor/", SensorConsumer.as_asgi()),
-    ]),
-})
