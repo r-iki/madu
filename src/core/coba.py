284d6d3f6d3a68b15cbba7b0e07c23ab1de25627
r-iki <riki.121110019@student.itera.ac.id>
2025-02-10 17:01:59 +0700

    Remove all __pycache__ files to clean up the project

--- src/core/coba.py
+++
@@ -1,9 +0,0 @@
-from django.core.mail import send_mail
-
-send_mail(
-    'Test Email dari Django',
-    'Halo, ini test email dari Django menggunakan Gmail.',
-    'rikimchd@gmail.com',
-    ['mail@riki.my.id'],
-    fail_silently=False,
-)
